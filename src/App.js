e8776392be9ef0c5f38da446e6ede9ca41762e41
Gabe Yuan <gabe@zq.com>
2023-03-16 15:02:40 +0800

    more simples

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
-import init, { add, greet } from "wasm-lib";
+import init, { helloworld, add, fibonacci, greet, print } from "wasm-lib";
+
+function fibonacciInJs(n) {
+  if (n <= 1) return n;
+  return fibonacciInJs(n - 1) + fibonacciInJs(n - 2);
+}
 
 function App() {
+  const [hello, setHello] = useState("");
   const [ans, setAns] = useState(0);
+  const [fib, setFib] = useState("");
+
   useEffect(() => {
     init().then(() => {
+      setHello(helloworld());
       setAns(add(1, 1));
+      print();
     });
   }, []);
+
+  const handleFib = async () => {
+    await init();
+
+    const num = 20;
+
+    console.time("Fibonnaci in rust");
+    const fibRust = fibonacci(num);
+    console.timeEnd("Fibonnaci in rust");
+
+    console.time("Fibonnaci in JS");
+    const fibJS = fibonacciInJs(num);
+    console.timeEnd("Fibonnaci in JS");
+
+    setFib(`Fib ${num}: ${fibRust}(rust), ${fibJS}(js)`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,7 +52,12 @@ function App() {
         >
           Learn React
         </a>
+        <p>{hello}</p>
         <p>1 + 1 = {ans}</p>
+        <p>
+          <button onClick={handleFib}>run fibonacci</button>
+        </p>
+        <p>{fib}</p>
         <p>
           <button
             onClick={() => {
--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,18 +1,42 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub fn helloworld() -> String {
+    String::from("Hello world from Rust!")
+}
+
+#[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
 #[wasm_bindgen]
-extern {
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 | 1 => n,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+#[wasm_bindgen]
+extern "C" {
     pub fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn print() {
+    log("Log from rust");
 }
 
 #[test]
